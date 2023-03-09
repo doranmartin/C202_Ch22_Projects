683d2bf227a7ecb7116da433fc77119d9d0cbb0a
doranmartin <doranmartin1994@gmail.com>
2023-03-09 17:15:43 -0500

    completed first project

--- src/Exercise22_01.java
+++ src/Exercise22_01.java
@@ -1,5 +1,32 @@
-public class Main {
+import java.sql.SQLOutput;
+import java.util.Scanner;
+public class Exercise22_01 {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.print("Enter a string: ");
+        Scanner scanner = new Scanner(System.in);
+        String text = scanner.next();
+
+        int start = 0;
+        String current, longest;
+        current = "";
+        longest = "";
+
+        for (int i = 1; i < text.length(); i++) {
+
+            if (text.charAt(i-1) >= text.charAt(i)) {
+                current = text.substring(start, i);
+                start = i;
+            }
+            else if (i == text.length() - 1) {
+                current = text.substring(start);
+            }
+
+            if (current.length() > longest.length()) {
+                longest = current;
+            }
+
+        }
+
+        System.out.println(longest);
     }
 }
